40e0ba91d02e8113524b43f80497bb606fa5d420
Nataliia Arsenieva <nataly.arsenieva@gmail.com>
2020-07-06 12:22:34 -0400

    Core Changes
    Init, Main, Start & Update functions added

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -6,10 +6,28 @@
         console.log("Initializing Start");
         var x = 10;
         var y = "Nataiia";
+        Start();
     }
     function Start() {
+        console.log("Starting Application...");
+        // Initialize CreateJS
         stage = new createjs.Stage(canvas);
         stage.enableMouseOver(20);
+        createjs.Ticker.framerate = 60;
+        createjs.Ticker.on("tick", Update);
+        Main();
+    }
+    function Update() {
+        stage.update();
+        helloLabel.scaleX += 0.001;
+        helloLabel.scaleY += 0.001;
+    }
+    function Main() {
+        console.log("Game Start");
+        helloLabel = new createjs.Text("Hello World!", "40px Consolas", "#000000");
+        helloLabel.x = 100;
+        helloLabel.y = 100;
+        stage.addChild(helloLabel);
     }
     window.onload = Init;
 })();
--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -8,12 +8,35 @@
         console.log("Initializing Start");
         let x: number = 10;
         let y: string = "Nataiia";
+        Start();
     }
 
     function Start() {
+        console.log("Starting Application...");
+
+        // Initialize CreateJS
         stage = new createjs.Stage(canvas);
         stage.enableMouseOver(20);
 
+        createjs.Ticker.framerate = 60;
+        createjs.Ticker.on("tick", Update);
+
+        Main();
+    }
+    function Update() {
+        stage.update();
+        helloLabel.scaleX += 0.001;
+        helloLabel.scaleY += 0.001;
+    }
+
+
+    function Main() {
+        console.log("Game Start");
+        helloLabel = new createjs.Text("Hello World!", "40px Consolas", "#000000");
+        helloLabel.x = 100;
+        helloLabel.y = 100;
+
+        stage.addChild(helloLabel);
     }
     window.onload = Init;
 })();
